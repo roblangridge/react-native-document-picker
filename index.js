2896b0c43474877b99a5ce3a2db10ce546619b29
Rob Langridge <langrir@gmail.com>
2018-11-09 16:35:29 +0000

    Gradle Build File

--- index.js
+++ index.js
@@ -1,55 +1,73 @@
-'use strict';
-import {Platform, NativeModules} from 'react-native';
-const {RNDocumentPicker} = NativeModules;
+"use strict";
+import { Platform, NativeModules } from "react-native";
+const { RNDocumentPicker } = NativeModules;
 
-if ( !RNDocumentPicker ) {
+if (!RNDocumentPicker) {
   // Use a timeout to ensure the warning is displayed in the YellowBox
   setTimeout(() => {
-    console.warn('RNDocumentPicker: Native module is not available, make sure you have finished the installation process and rebuilt your app');
+    console.warn(
+      "RNDocumentPicker: Native module is not available, make sure you have finished the installation process and rebuilt your app"
+    );
   }, 0);
-} else if ( !RNDocumentPicker.pick && RNDocumentPicker.show ) {
+} else if (!RNDocumentPicker.pick && RNDocumentPicker.show) {
   // Use a timeout to ensure the warning is displayed in the YellowBox
   setTimeout(() => {
-    console.warn('RNDocumentPicker: Native module is obsolete, you may not have rebuilt your app after upgrading the library');
+    console.warn(
+      "RNDocumentPicker: Native module is obsolete, you may not have rebuilt your app after upgrading the library"
+    );
   }, 0);
 }
 
-const E_DOCUMENT_PICKER_CANCELED = 'DOCUMENT_PICKER_CANCELED';
+const E_DOCUMENT_PICKER_CANCELED = "DOCUMENT_PICKER_CANCELED";
 
 function pick(opts) {
-  if ( 'filetype' in opts ) {
-    throw new TypeError('A `filetype` option was passed to DocumentPicker.pick, the correct option is `type`');
+  if ("filetype" in opts) {
+    throw new TypeError(
+      "A `filetype` option was passed to DocumentPicker.pick, the correct option is `type`"
+    );
   }
-  if ( 'types' in opts ) {
-    throw new TypeError('A `types` option was passed to DocumentPicker.pick, the correct option is `type`');
+  if ("types" in opts) {
+    throw new TypeError(
+      "A `types` option was passed to DocumentPicker.pick, the correct option is `type`"
+    );
   }
 
-  if ( !('type' in opts) ) {
+  if (!("type" in opts)) {
     opts.type = DocumentPicker.types.allFiles;
   }
 
-  opts.type = Array.isArray(opts.type) ? opts.type : [opts.type]
+  opts.type = Array.isArray(opts.type) ? opts.type : [opts.type];
 
-  if ( opts.type.some((type) => type === undefined) ) {
-    throw new TypeError('Unexpected undefined type option, did you try using a DocumentPicker.types.* that does not exist?');
+  if (opts.type.some(type => type === undefined)) {
+    throw new TypeError(
+      "Unexpected undefined type option, did you try using a DocumentPicker.types.* that does not exist?"
+    );
   }
 
-  if ( Array.isArray(opts.type) && opts.type.length < 1 ) {
-    throw new TypeError('`type` option should not be an empty array, at least one type must be passed if the `type` option is not omitted');
-  }
-  
-  static video() {
-    return (Platform.OS === 'android') ? "video/*" : "public.video";
+  if (Array.isArray(opts.type) && opts.type.length < 1) {
+    throw new TypeError(
+      "`type` option should not be an empty array, at least one type must be passed if the `type` option is not omitted"
+    );
   }
 
-  opts.type.forEach((type) => {
-    if ( typeof type !== 'string' ) {
-      throw new TypeError('Invalid type option, expected a string not: ' + type);
+  opts.type.forEach(type => {
+    if (typeof type !== "string") {
+      throw new TypeError(
+        "Invalid type option, expected a string not: " + type
+      );
     }
   });
 
-  if ( opts.type.length > 1 && Platform.OS === 'android' && Platform.Version < 19 ) {
-    console.warn(`RNDocumentPicker: Android API level ${Platform.Version} does not support multiple types, falling back to */*`);
+  if (
+    opts.type.length > 1 &&
+    Platform.OS === "android" &&
+    Platform.Version < 19
+  ) {
+    console.warn(
+      `RNDocumentPicker: Android API level ${
+        Platform.Version
+      } does not support multiple types, falling back to */*`
+    );
   }
 
   return RNDocumentPicker.pick(opts);
@@ -57,28 +75,29 @@ function pick(opts) {
 
 const Types = {
   mimeTypes: {
-    'allFiles': '*/*',
-    'audio': 'audio/*',
-    'images': 'image/*',
-    'plainText': 'text/plain',
-    'pdf': 'application/pdf',
-    'video': 'video/*',
+    allFiles: "*/*",
+    audio: "audio/*",
+    images: "image/*",
+    plainText: "text/plain",
+    pdf: "application/pdf",
+    video: "video/*"
   },
   utis: {
-    'allFiles': 'public.content',
-    'audio': 'public.audio',
-    'images': 'public.image',
-    'plainText': 'public.plain-text',
-    'pdf': 'com.adobe.pdf',
-    'video': 'public.video',
+    allFiles: "public.content",
+    audio: "public.audio",
+    images: "public.image",
+    plainText: "public.plain-text",
+    pdf: "com.adobe.pdf",
+    video: "public.video"
   },
   extensions: {
-    'allFiles': '*',
-    'audio': '.3g2 .3gp .aac .adt .adts .aif .aifc .aiff .asf .au .m3u .m4a .m4b .mid .midi .mp2 .mp3 .mp4 .rmi .snd .wav .wax .wma',
-    'images': '.jpeg .jpg .png',
-    'plainText': '.txt',
-    'pdf': '.pdf',
-    'video': '.mp4',
+    allFiles: "*",
+    audio:
+      ".3g2 .3gp .aac .adt .adts .aif .aifc .aiff .asf .au .m3u .m4a .m4b .mid .midi .mp2 .mp3 .mp4 .rmi .snd .wav .wax .wma",
+    images: ".jpeg .jpg .png",
+    plainText: ".txt",
+    pdf: ".pdf",
+    video: ".mp4"
   }
 };
 
@@ -94,12 +113,12 @@ export default class DocumentPicker {
    *
    * @see https://developer.apple.com/library/ios/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html
    */
-  static types = PlatformTypes[Platform.OS] || Types.mimeTypes
+  static types = PlatformTypes[Platform.OS] || Types.mimeTypes;
 
   static pick(opts) {
     opts = opts || {};
     opts.multiple = false;
-    return pick(opts).then((results) => results[0]);
+    return pick(opts).then(results => results[0]);
   }
 
   static pickMultiple(opts) {
